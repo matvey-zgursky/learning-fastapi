8a1ca0c36ae21c3ead1d13fcb0628830ced3f0d8
matvey-zgursky <matveyzgursky@gmail.com>
2025-02-01 17:03:54 +0700

    lesson 2: Создание БД с помощью pydantic

--- surenkhorenyan/main.py
+++ surenkhorenyan/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
-
+from contextlib import asynccontextmanager
 
 from items_views import router as items_router
 from users.views import router as users_router
+from core.models import Base, db_helper
 
-app = FastAPI()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(items_router)
 app.include_router(users_router)
 
