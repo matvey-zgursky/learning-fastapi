23b4e8389354f8e486dd7b1f5015d7172bcac40b
matvey-zgursky <matveyzgursky@gmail.com>
2025-07-01 16:47:20 +0700

    использование простой связки many-to-many

--- surenkhorenyan/crud.py
+++ surenkhorenyan/crud.py
@@ -123,33 +123,9 @@ async def create_product(session: AsyncSession, name: str, description: str,
     session.add(product)
     await session.commit()
     return product
+    
 
-
-async def main_relations(session: AsyncSession):
-    await create_user(session=session, username='John')
-    await create_user(session=session, username='alice')
-    await create_user(session=session, username='Sam')
-    user_sam = await get_user_by_username(session=session, username='Sam')
-    user_john = await get_user_by_username(session=session, username='John')
-    # user_bob = await get_user_by_username(session=session, username='Bob')
-    await create_user_profile(session=session,
-                              user_id=user_john.id,
-                              first_name='John')
-    await create_user_profile(session=session,
-                              user_id=user_sam.id,
-                              first_name='Sam',
-                              last_name='White')
-    await show_users_with_profiles(session=session)
-    await create_posts(session, user_john.id, 'SQLA 2.0', 'SQLA Joins')
-    await create_posts(session, user_sam.id, 'FastAPI intro',
-                       'FasAPI Advanced', 'FastAPI more')
-    await get_users_with_posts(session=session)
-    await get_posts_with_authors(session=session)
-    await get_users_with_posts_and_profiles(session=session)
-    await get_profiles_with_users_and_users_with_posts(session=session)
-
-
-async def demo_m2m(session: AsyncSession):
+async def create_orders_and_products(session: AsyncSession):
     order_one = await create_order(session=session)
     order_promo = await create_order(session=session, promocode='promo')
 
@@ -189,6 +165,50 @@ async def demo_m2m(session: AsyncSession):
     order_promo.products.append(display)
 
     await session.commit()
+
+
+async def get_orders_with_products(session: AsyncSession) -> list[Order]:
+    stmt = (
+        select(Order)
+        .options(selectinload(Order.products))
+        .order_by(Order.id)
+        )
+    
+    orders = await session.scalars(stmt)
+
+    return list(orders)
+
+async def main_relations(session: AsyncSession):
+    await create_user(session=session, username='John')
+    await create_user(session=session, username='alice')
+    await create_user(session=session, username='Sam')
+    user_sam = await get_user_by_username(session=session, username='Sam')
+    user_john = await get_user_by_username(session=session, username='John')
+    # user_bob = await get_user_by_username(session=session, username='Bob')
+    await create_user_profile(session=session,
+                              user_id=user_john.id,
+                              first_name='John')
+    await create_user_profile(session=session,
+                              user_id=user_sam.id,
+                              first_name='Sam',
+                              last_name='White')
+    await show_users_with_profiles(session=session)
+    await create_posts(session, user_john.id, 'SQLA 2.0', 'SQLA Joins')
+    await create_posts(session, user_sam.id, 'FastAPI intro',
+                       'FasAPI Advanced', 'FastAPI more')
+    await get_users_with_posts(session=session)
+    await get_posts_with_authors(session=session)
+    await get_users_with_posts_and_profiles(session=session)
+    await get_profiles_with_users_and_users_with_posts(session=session)
+
+
+async def demo_m2m(session: AsyncSession):
+    # await create_orders_and_products(session=session)
+    orders = await get_orders_with_products(session=session)
+    for order in orders:
+        print(order.id, order.promocode, order.created_at, 'product:')
+        for product in order.products:
+            print('-', product.id, product.name, product.price)
 
 
 async def main():
