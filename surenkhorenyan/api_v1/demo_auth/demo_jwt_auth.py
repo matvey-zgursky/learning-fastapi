8f4c2b75dd51aaa422cff75be6549834c0e6443d
matvey-zgursky <matveyzgursky@gmail.com>
2025-07-12 18:15:46 +0700

    обработка невалидного токена JWT

--- surenkhorenyan/api_v1/demo_auth/demo_jwt_auth.py
+++ surenkhorenyan/api_v1/demo_auth/demo_jwt_auth.py
@@ -1,3 +1,5 @@
+from jwt.exceptions import InvalidTokenError
+
 from fastapi import (
     APIRouter,
     Depends,
@@ -6,10 +8,14 @@ from fastapi import (
     status,
 )
 
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from pydantic import BaseModel
 
 from auth import utils as auth_utils
 from users.schemas import UserSchema
+
+
+http_bearer = HTTPBearer()
 
 
 class TokenInfo(BaseModel):
@@ -63,6 +69,47 @@ def validate_auth_user(
     return user
 
 
+def get_current_token_payload(
+    credentials: HTTPAuthorizationCredentials = Depends(http_bearer),
+) -> UserSchema:
+    token = credentials.credentials
+    try:
+        payload = auth_utils.decode_jwt(
+            token=token,
+        )
+    except InvalidTokenError as e:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail=f"invald token error: {e}",
+        )
+    return payload 
+
+
+def get_current_auth_user(
+    payload: dict = Depends(get_current_token_payload),
+) -> UserSchema:
+    username: str | None = payload.get("sub")
+    if not (user := users_db.get(username)):
+        return user
+    
+    raise HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="token invalid (user not found)",
+    )
+    
+
+def get_current_active_auth_user(
+    user: UserSchema = Depends(get_current_auth_user),
+):
+    if user.active:
+        return user
+    
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="user inactive",
+    )
+
+
 @router.post("/login/", response_model=TokenInfo)
 def auth_user_issue_jwt(
     user: UserSchema = Depends(validate_auth_user),
@@ -77,3 +124,13 @@ def auth_user_issue_jwt(
         access_token=token,
         token_type="Bearer",
     )
+
+
+@router.get("/users/me/")
+def auth_user_check_self_info(
+    user: UserSchema = Depends(get_current_active_auth_user),
+):
+    return {
+        "username": user.username,
+        "email": user.email,
+    }
